15a61b8e9296eba6f53410e695363522af44bf72
Lucas Aioria Serpa <Lucasaioriaserpa@outlook.com>
2026-03-25 21:51:36 -0300

    Creation and otmization of the player coins!

--- SRC/VIEWS/Main_screen_view.py
+++ SRC/VIEWS/Main_screen_view.py
@@ -7,6 +7,7 @@ class Main_screen_view(ctk.CTk, View):
     __log = Logger()
 
     PAD = 10
+    SIZE_INPUT_COINS = ({"x": 40, "y": 30})
     COIN_TYPES = [
         "cobres",
         "pratas",
@@ -74,43 +75,30 @@ class Main_screen_view(ctk.CTk, View):
         self.__make_player_coins_input(frame_coins)
 
     def __make_player_coins(self, main_frame: ctk.CTkFrame):
-        frame_player_coins = self.___make_frame_with_title("Moedas Atuais!", ("Helvetica", 20), main_frame)
+        frame_player_coins = self.___make_frame_with_title(
+            title="Moedas Atuais!",
+            font=("Helvetica", 20),
+            main_frame=main_frame,
+            row=0, column=0
+        )
         
-        ROW_COIN_TYPE = 2
-        for i in range(len(self.COINS)):
-            COIN_TYPE = self.COIN_TYPES[i]
-            COIN_SUB_TYPE_OBJ = list(self.COINS.values())[i]
-
-            type_text = ctk.CTkLabel(frame_player_coins, text=COIN_TYPE.upper())
-            type_text.grid(row=ROW_COIN_TYPE, column=0)
-
-            ROW_COIN_TYPE = ROW_COIN_TYPE + 1
-
-            frame_sub_coins = ctk.CTkFrame(frame_player_coins)
-            frame_sub_coins.grid(row=ROW_COIN_TYPE, column=0)
-
-            for y in range(len(COIN_SUB_TYPE_OBJ)):
-                COIN_SUB_TYPE = list(COIN_SUB_TYPE_OBJ.keys())[y]
-                COIN_VALUE = list(COIN_SUB_TYPE_OBJ.values())[y]
-                ROW_SUB_COIN_TYPE = ROW_COIN_TYPE + y + 1
-
-                subType_text = ctk.CTkLabel(frame_sub_coins, text=f"{COIN_SUB_TYPE}: ")
-                subType_text.grid(row=ROW_SUB_COIN_TYPE, column=0, padx=self.PAD, sticky="nsew")
-
-                value = ctk.CTkLabel(frame_sub_coins, text=f"{COIN_VALUE}")
-                value.grid(row=ROW_SUB_COIN_TYPE, column=1, padx=self.PAD, sticky="nsew")
-
-            ROW_COIN_TYPE = ROW_SUB_COIN_TYPE + 1
+        self.___make_labels_inputs_coins(frame_player_coins, True)
 
     def __make_player_coins_input(self, main_frame: ctk.CTkFrame):
-        frame_player_coins_input = self.___make_frame_with_title("Inserção de moeda", ("Helvetica", 20), main_frame)
+        frame_player_coins_input = self.___make_frame_with_title(
+            title="Inserção de moeda",
+            font=("Helvetica", 20),
+            main_frame=main_frame,
+            row=0, column=1
+        )
 
-    
-    def ___make_frame_with_title(self, title: str, font: tuple[str, int], main_frame: ctk.CTkFrame) -> ctk.CTkFrame:
+        self.___make_labels_inputs_coins(frame_player_coins_input, False)
+
+    def ___make_frame_with_title(self, title: str, font: tuple[str, int], main_frame: ctk.CTkFrame, row: int, column: int) -> ctk.CTkFrame:
         frame = ctk.CTkFrame(main_frame)
         frame.grid(
-            row=0,
-            column=0,
+            row=row,
+            column=column,
             padx=self.PAD, pady=self.PAD,
             sticky="nsew"
         )
@@ -119,6 +107,42 @@ class Main_screen_view(ctk.CTk, View):
         title_label.grid(row=1, column=0, sticky="nsew")
 
         return frame
+
+    def ___make_labels_inputs_coins(self, frame: ctk.CTkFrame, activate_labels: bool):
+        ROW_COIN_TYPE = 2
+        for i in range(len(self.COINS)):
+            COIN_TYPE = self.COIN_TYPES[i]
+            COIN_SUB_TYPE_OBJ = list(self.COINS.values())[i]
+            
+            type_text = ctk.CTkLabel(frame, text=COIN_TYPE.upper())
+            type_text.grid(row=ROW_COIN_TYPE, column=0)
+            
+            ROW_COIN_TYPE = ROW_COIN_TYPE + 1
+            
+            frame_sub_coins = ctk.CTkFrame(frame)
+            frame_sub_coins.grid(row=ROW_COIN_TYPE, column=0, padx=self.PAD, sticky="nsew")
+            
+            for y in range(len(COIN_SUB_TYPE_OBJ)):
+                COIN_SUB_TYPE = list(COIN_SUB_TYPE_OBJ.keys())[y]
+                COIN_VALUE = list(COIN_SUB_TYPE_OBJ.values())[y]
+                ROW_SUB_COIN_TYPE = ROW_COIN_TYPE + y + 1
+                
+                subType_text = ctk.CTkLabel(frame_sub_coins, text=f"{COIN_SUB_TYPE}: ")
+                subType_text.grid(row=ROW_SUB_COIN_TYPE, column=0, padx=self.PAD, sticky="nsew")
+                
+                if activate_labels:
+                    value = ctk.CTkLabel(frame_sub_coins, text=COIN_VALUE)
+                    value.grid(row=ROW_SUB_COIN_TYPE, column=1, padx=self.PAD, sticky="nsew")
+                    continue
+                
+                input_ = ctk.CTkTextbox(
+                    frame_sub_coins,
+                    activate_scrollbars=False,
+                    width=self.SIZE_INPUT_COINS["x"], height=self.SIZE_INPUT_COINS["y"]
+                )
+                input_.grid(row=ROW_SUB_COIN_TYPE, column=1, padx=self.PAD, sticky="nsew")
+
+            ROW_COIN_TYPE = ROW_SUB_COIN_TYPE + 1
 
     def _make_options(self):
         frame_buttons = ctk.CTkFrame(self.mainFrame)
